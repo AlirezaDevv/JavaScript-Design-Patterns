b350d8a888ead551fc4893116bee6b26b4827a09
Alireza Mirzaee <130092310+AlirezaDevv@users.noreply.github.com>
2024-06-08 19:31:28 +0000

    prototype pattern

---
+++ PrototypePattern.js
@@ -0,0 +1,30 @@
+class Dog {
+    constructor(name) {
+      this.name = name;
+    }
+  
+    bark() {
+      return `Woof!`;
+    }
+  }
+  
+  const dog1 = new Dog("Daisy");
+  const dog2 = new Dog("Max");
+  const dog3 = new Dog("Spot");
+  
+  Dog.prototype.play = () => console.log("Playing now!");
+  
+  dog1.play();
+
+
+
+  const dog = {
+    bark() {
+      return `Woof!`;
+    },
+  };
+  
+  const pet1 = Object.create(dog);
+
+  console.log(pet1)
+  console.log(pet1.bark())
